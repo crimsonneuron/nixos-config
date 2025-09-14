5f33cadaa7025df590410ad200da57eb967d4221
crimsonneuron <james.r.devereux@gmail.com>
2025-09-14 04:14:50 +0000

    added laptop config

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,216 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+      # ../../modules/nixos/games.nix
+      ../../modules/nixos/keyboard.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "lsla"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  #services.xserver.enable = true;
+
+  services.displayManager.ly.enable=true;
+  services.udisks2.enable=true;
+
+  hardware.graphics.enable=true;
+
+  programs.niri.enable = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true; # Show battery charge of Bluetooth devices
+      };
+    };
+  };
+  
+
+  services.blueman.enable=true;
+
+
+
+
+  #XRDP Remote Desktop Server setup
+   #services.xrdp = {
+     #enable= true;
+     #defaultWindowManager = "gnome-remote-desktop";
+     #audio.enable =true;
+     #openFirewall = true;
+ #
+   #};
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.crimson = {
+    isNormalUser = true;
+    description = "James Devereux";
+    extraGroups = [ "networkmanager" "wheel" "video" "input" "audio" "render"];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+    shell = pkgs.zsh;
+    hashedPassword = "$6$FmPqvbAaTymXAgCG$4zeToftuip2hp1MlKS7Na3FTQd4PevFp5ziUEvrJ5wBHXZUcFJZdSIc1pqd3Pu2EklKShARQ3pLEd4CkRUtl2";
+  };
+    home-manager = {
+      extraSpecialArgs = {inherit inputs;};
+      users = {
+        "crimson" = import ./home.nix;
+    };
+    backupFileExtension = "bkup";
+  };
+  
+  programs.zsh = {
+    enable=true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  nix.extraOptions = "experimental-features = nix-command flakes";
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    alacritty
+    ranger
+    p7zip
+
+    gcc
+    gnumake
+    cmake
+    pkg-config
+
+    home-manager
+    
+    gnome-remote-desktop
+
+    #distrobox
+    #podman
+
+    #rocm:
+
+    #Games
+    #Niri:
+    xwayland-satellite
+
+  ];
+
+  systemd.user.services."init-session" = {
+      description = "Initialize systemd user session for Wayland";
+      wantedBy = [ "default.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.systemd}/bin/systemd --user";
+        # Critical: Don't fork! Must stay attached to TTY
+        Type = "simple";
+        # Ensure it runs BEFORE compositor
+        Before = [ "sway.service" "niri.service" ];
+      };
+    };
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+
+    fileSystems."/mnt/storage" = {
+      device = "/dev/disk/by-uuid/6ea6f707-199d-4227-a89f-9d696d44cd89";
+      fsType = "ext4";
+      options = [ "defaults" "nofail" "exec" "noatime"  "discard"];
+    };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  security.polkit.enable=true;
+  security.pam.services.hyprlock={};
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
---
+++ hosts/laptop/home.nix
@@ -0,0 +1,388 @@
+{ config, pkgs,inputs,  ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "crimson";
+  home.homeDirectory = "/home/crimson";
+  nixpkgs.config.allowUnfree = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  imports = [
+    inputs.zen-browser.homeModules.beta
+    inputs.spicetify-nix.homeManagerModules.default 
+    #inputs.ssbm-nix.homeManagerModule
+    ../../modules/home-manager/games.nix
+    ../../modules/home-manager/scripts.nix
+    ../../modules/home-manager/hyprlock.nix
+  ];
+
+  home.packages = with pkgs; [
+    (discord.override {
+      withOpenASAR = true;
+      withVencord = false;
+    })
+    obsidian
+    cutechess
+    beeper
+    youtube-music
+    ncmpcpp
+
+    libqalculate
+
+
+    vlc
+    yt-dlp
+    
+    rustc
+    cargo
+    rust-analyzer
+
+    wl-clipboard
+    python313
+    nil
+
+    tree-sitter
+    unzip
+    wget
+    curl
+    neofetch
+    jq
+    btop
+    fzf
+    swaynotificationcenter
+    keyd
+ 
+    libnotify
+
+    #gnome-tweaks
+    swaybg
+    pavucontrol
+    networkmanagerapplet
+    font-awesome
+    nerd-fonts.code-new-roman
+    nerd-fonts.fantasque-sans-mono
+
+    mesa-demos
+    feh
+    icoutils
+    jq
+    pywal
+    tofi
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+
+  ];
+
+  programs = {
+    ncmpcpp = {
+      enable=true;
+      mpdMusicDir = /mnt/storage/music;
+    };
+    distrobox = {
+     enable=true;
+     enableSystemdUnit = true;
+     containers = {
+       ubunturocm = {
+         image = "docker.io/library/ubuntu:latest";
+         init_hooks = [
+          "export LD_LIBRARY_PATH=\"/opt/rocm-6.4.1/lib:\$LD_LIBRARY_PATH\""
+         ];
+       };
+     };
+     };   
+    neovim = {
+      enable=true;
+      defaultEditor=true;
+
+      plugins = with pkgs.vimPlugins; [
+        #nvim plugins:
+        lualine-nvim
+        nvim-web-devicons
+    
+        kanagawa-nvim
+        
+        tagbar
+        indentLine
+        vim-fugitive
+        nvim-autopairs
+        nvim-lspconfig
+        rust-tools-nvim
+        nvim-cmp
+        cmp-nvim-lsp
+        cmp-nvim-lua
+        cmp-nvim-lsp-signature-help
+        cmp-vsnip
+        cmp-path
+        cmp-buffer
+        vim-vsnip
+        nvim-treesitter.withAllGrammars
+       ];
+      extraPackages = with pkgs.tree-sitter-grammars; [
+        tree-sitter-nix
+        tree-sitter-rust
+        tree-sitter-lua
+        tree-sitter-python
+        tree-sitter-json
+            
+      ];
+  };
+    zsh = {
+      enable=true;
+      
+      shellAliases = {
+        rebuild = "sudo ~/nixos/scripts/bash/rebuild-commit.sh";
+        cleanup = "sudo nix-collect-garbage -d";
+        test = "sudo nixos-rebuild test --flake ~/nixos#desktop";
+        # homerebuild = "home-manager switch --flake ~/nixos#desktop";
+      };
+      initExtra = " 
+        export EDITOR=nvim
+        export VISUAL=nvim
+        export DISPLAY=:0
+        " ;
+};
+  zen-browser= {
+   enable=true;
+   policies = {
+     DisableAppUpdate = true;
+     DisableTelemetry = true;
+   };
+};
+  lazygit = {
+     enable=true;
+  };
+  git = {
+    enable=true;
+    userName="crimsonneuron";
+    userEmail="james.r.devereux@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      core.editor="nvim";
+    };
+    delta.enable=true;
+  };
+  spicetify = 
+    let 
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in 
+    {
+      enable = true;
+
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+      ];
+      enabledCustomApps = with spicePkgs.apps; [
+        newReleases
+        ncsVisualizer
+      ];
+      enabledSnippets = with spicePkgs.snippets; [
+        rotatingCoverart
+        pointer
+      ];
+
+      theme = spicePkgs.themes.catppuccin;
+      colorScheme = "mocha";
+      };
+
+    swaylock.enable=true;
+    waybar = {
+      enable=true;
+      #systemd.enable=true;
+    };
+  };
+
+  games = {
+    enable=true;
+    melee=true;
+  };
+
+
+
+   #slippi-launcher= {
+     #isoPath = "/mnt/storage/games/Slippi/isos/Super Smash Bros. Melee (USA) (En,Ja) (v1.02).iso";
+ #
+     ##default path: /mnt/storage/games/Slippi/Super Smash Bros. Melee (USA) (En,Ja) (v1.02).iso
+     ##20xx path: /mnt/storage/games/Slippi/isos/SSBM, 20XXHP 5.0.2.iso
+     #launchMeleeOnPlay = true;
+     #rootSlpPath = "/mnt/storage/games/Slippi/replays/";
+     #useMonthlySubfolders  =true;
+   #};
+
+   #ssbm.slippi-launcher = {
+     #enable=true;
+     #isoPath = "/mnt/storage/games/Slippi/isos/Super Smash Bros. Melee (USA) (En,Ja) (v1.02).iso";
+   #};
+
+  services.podman = {
+    enable=true;
+  };
+  services.udiskie = {
+    enable = true;
+    settings ={
+      program_options = {
+        file_manager = "${pkgs.alacritty}/bin/alacritty -e ${pkgs.ranger}/bin/ranger";
+      };
+    };
+  };
+  services.mpd = {
+    enable=true;
+    musicDirectory = /mnt/storage/music;
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "My PipeWire Output"
+      }
+    '';
+  };
+
+  services.swayidle.enable=true;
+  services.polkit-gnome.enable=true;
+  
+
+   #systemd.user.services = {
+     #xwayland-satellite = {
+       #Unit = {
+         #Description = "Thing for doing x11 only things";
+         #BindsTo = ["graphical-session.target"];
+         #PartOf = ["graphical-session.target"];
+         #After = ["graphical-session.target"];
+         #Requisite = ["graphical-session.target"];
+       #};
+       #Service = {
+         #Type ="notify";
+         #NotifyAccess= "all";
+         #ExecStart = "${pkgs.xwayland-satellite}/bin/xwayland-satellite :0";
+         #Restart = "on-failure";
+         #Environment = "DISPLAY=:0";
+       #};
+       #Install = {
+         #WantedBy = ["graphical-session.target"];
+       #};
+     #};
+ #
+     #swaybg = {
+       #Unit = {
+         #Description = "swaybg background setting";
+         #BindsTo = ["graphical-session.target"];
+         #PartOf = ["graphical-session.target"];
+         #After = ["graphical-session.target"];
+         #Requisite = ["graphical-session.target"];
+       #};
+ #
+       #Service = {
+         #ExecStart = "${pkgs.swaybg}/bin/swaybg -i /home/crimson/Pictures/Wallpapers/nasa_mirror_1920x1080.png";
+       #};
+       #Install = {
+         #WantedBy = ["graphical-session.target"];
+       #};
+     #};
+  #};
+ #
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+    
+    
+  };
+  
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/crimson/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    DISPLAY = ":0";
+  };
+  
+  xdg.configFile = {
+    "nvim/init.lua".source = ../../dotfiles/nvim/init.lua;
+    "nvim/lua".source = ../../dotfiles/nvim/lua;
+    "niri/config.kdl".source = ../../dotfiles/niri/laptop-config.kdl;
+
+    "waybar/config.jsonc".source = ../../dotfiles/waybar/config.jsonc;
+    "waybar/style.css".source = ../../dotfiles/waybar/style.css;
+
+    "containers/registries.conf".text = ''
+      [registries.search]
+      registries = ['docker.io', 'quay.io']
+    '';
+    "containers/policy.json".text = ''
+      {
+        "default": [
+          {
+            "type": "insecureAcceptAnything"
+          }
+        ]
+      }
+    '';
+    "containers/storage.conf".text = ''
+      [storage]
+      driver = "overlay"
+      graphroot = "/mnt/storage/distrobox"
+    '';
+    "fontconfig/conf.d/100-nix.conf".text = ''
+      <?xml version="1.0"?>
+      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+      <fontconfig>
+        <dir>~/.nix-profile/lib/X11/fonts</dir>
+        <dir>~/.nix-profile/share/fonts</dir>
+      </fontconfig>
+    '';
+    "tofi/config".source = ../../dotfiles/tofi/fullscreen;
+
+  };
+
+
+
+ #dconf.settings = {
+     #"org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+         #name = "Launch Alacritty";
+         #command = "alacritty";
+         #binding = "<Super>Return"; 
+     #};
+   #};
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
