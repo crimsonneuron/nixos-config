6d769bda986266532ed055b75282cecc26cbcdc5
crimsonneuron <james.r.devereux@gmail.com>
2025-09-18 18:15:17 -0400

    trying again on fingerprints

--- modules/home-manager/hyprlock.nix
+++
@@ -1,8 +0,0 @@
-{config, pkgs, ...}:
-
-{
-  programs.hyprlock = {
-    enable=true;
-  };
-  xdg.configFile."hypr/hyprlock.conf".source = ../../dotfiles/hyprlock/config.conf;
-}
---
+++ modules/home-manager/utils/hyprlock.nix
@@ -0,0 +1,8 @@
+{config, pkgs, ...}:
+
+{
+  programs.hyprlock = {
+    enable=true;
+  };
+  xdg.configFile."hypr/hyprlock.conf".source = ../../dotfiles/hyprlock/laptop-config.conf;
+}
