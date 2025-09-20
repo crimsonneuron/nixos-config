43b3450e3eec19f7b4463ce520799c98fe83d5f1
crimsonneuron <james.r.devereux@gmail.com>
2025-09-20 13:54:25 -0400

    hmm

--- modules/home-manager/utils/default.nix
+++ modules/home-manager/utils/default.nix
@@ -4,12 +4,12 @@ let
   # Get all .nix files in the current directory except default.nix itself
   nixFiles = builtins.filter (name: 
     name != "default.nix" && 
-    inputs.nixpkgs.lib.hasSuffix ".nix" name
+    builtins.match ".*\\.nix$" name != null
   ) (builtins.attrNames (builtins.readDir ./.));
 
   # Import each .nix file and create an attribute set
   importedModules = builtins.listToAttrs (map (file: {
-    name = inputs.nixpkgs.lib.removeSuffix ".nix" file;
+    name = builtins.substring 0 (builtins.stringLength file - 4) file;
     value = import (./. + "/${file}") inputs;
   }) nixFiles);
 
