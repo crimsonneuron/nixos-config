567a75c77d23ff597322d575b49cfd5e53b8e9bd
crimsonneuron <james.r.devereux@gmail.com>
2025-09-21 15:25:01 -0400

    what if whoever made this was smart

--- modules/home-manager/utils/waybar/cava.nix
+++ modules/home-manager/utils/waybar/cava.nix
@@ -11,18 +11,11 @@
       
       input.method = "pipewire";
       input.source= "auto";
-      input.sensitivity= 10;
 
       output.method = "raw";
       output.raw_target = "/dev/stdout";
       output.bit_format = "8bit";
       output.ascii_max_range = 7;
-
-      smoothing.monstercat = 1;
-      smoothing.waves = 0;
-      smoothing.noise-reduction =0.95;
-      smoothing.gravity = 200;
-      smoothing.ignore =0;
     };
   };
 }
