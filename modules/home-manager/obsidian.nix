7bd9e497a88831f8930c07cb7780757b7d508279
crimsonneuron <james.r.devereux@gmail.com>
2025-10-21 11:42:19 -0400

    hopefully it'll auto-create the vault every time I rebuild now

--- modules/home-manager/obsidian.nix
+++ modules/home-manager/obsidian.nix
@@ -1,8 +1,15 @@
-{pkgs, ...}:
+{pkgs,lib, ...}:
 
 {
   home.packages = with pkgs; [
     obsidian
     veracrypt
   ];
+
+  home.activation.createObsidianVault = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    cat > $HOME/.config/obsidian/vaults.json << 'EOF'
+    {"vaults":{"fbb49e6f697f76b6":{"path":"/run/media/veracrypt1/strongroom","ts":1760900415,"open":true}}}
+    EOF
+    '';
 }
+
