8b972defe12427490a78a81c330cb3c53ea1172d
crimsonneuron <james.r.devereux@gmail.com>
2025-10-21 18:31:42 -0400

    wrong file to target

--- modules/home-manager/obsidian.nix
+++ modules/home-manager/obsidian.nix
@@ -7,7 +7,7 @@
   ];
 
   home.activation.createObsidianVault = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    cat > $HOME/.config/obsidian/vaults.json << 'EOF'
+    cat > $HOME/.config/obsidian/obsidian.json << 'EOF'
     {"vaults":{"fbb49e6f697f76b6":{"path":"/run/media/veracrypt1/strongroom","ts":1760900415,"open":true}}}
     EOF
     '';
