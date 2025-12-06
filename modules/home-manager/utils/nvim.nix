92eb5002edc1ae789f66db28cd98d61c9157dd38
crimsonneuron <james.r.devereux@gmail.com>
2025-12-06 14:13:46 -0500

    god nixvim be nice

--- modules/home-manager/utils/nvim.nix
+++ modules/home-manager/utils/nvim.nix
@@ -128,12 +128,6 @@
       lsp = {
         enable = true;
         servers = {
-          rust_analyzer = {
-            enable = true;
-            installCargo = false;
-            installRustc = false;
-          };
-          nil_ls = {
             enable = true;
           };
            #qmlls = {
