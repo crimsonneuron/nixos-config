95227a93334da16d6fe9c73eb6af53d6d1350688
crimsonneuron <james.r.devereux@gmail.com>
2026-01-20 22:47:58 -0500

    cachix is still odd to me

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -148,6 +148,12 @@
   nix = {
     extraOptions = "experimental-features = nix-command flakes";
     settings = {
+      substituters = [
+        "https://nix-gaming.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      ];
       extra-substituters = [
         "https://vicinae.cachix.org"
       ];
