a8562b1c3af49fef3d48dadedfe8d6f89d9ab844
crimsonneuron <james.r.devereux@gmail.com>
2026-02-22 23:32:59 -0500

    sigh imported the wrong thing

--- modules/nixos/slippi.nix
+++ modules/nixos/slippi.nix
@@ -2,7 +2,7 @@
 #See the HM module games.nix for the actual config of this, its just that
 #The gcc adapter needs to be in a nix file. :\
 {
-  imports  = [inputs.ssbm-nix.overlay];
+  imports  = [inputs.ssbm-nix.nixosModule];
   ssbm.gcc = {
     rules.enable =true;
     oc-kmod.enable =true;
