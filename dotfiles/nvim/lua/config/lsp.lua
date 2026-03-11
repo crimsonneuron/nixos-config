81e0e5f39e49b85f69c5e39e4923c952e7920efc
crimsonneuron <james.r.devereux@gmail.com>
2026-03-11 00:45:25 -0400

    bad commit

--- dotfiles/nvim/lua/config/lsp.lua
+++ dotfiles/nvim/lua/config/lsp.lua
@@ -5,5 +5,8 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- nil_ls: Nix LSP
 vim.lsp.config("nil_ls", {capabilities=capabilities,})
 vim.lsp.enable("nil_ls")
+
+vim.lsp.config("ocamllsp" , {capabilities=capabilities,})
+vim.lsp.enable("nil_ls")
 -- rustaceanvim sets up rust-analyzer on its own via its plugin;
 -- do NOT call lspconfig.rust_analyzer.setup() here.
