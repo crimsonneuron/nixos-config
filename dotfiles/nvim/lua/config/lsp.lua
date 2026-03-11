f7dacb04423fa1bb16983cd08844665d1ceb6c12
crimsonneuron <james.r.devereux@gmail.com>
2026-03-11 00:36:47 -0400

    some warning

--- dotfiles/nvim/lua/config/lsp.lua
+++ dotfiles/nvim/lua/config/lsp.lua
@@ -1,12 +1,9 @@
 -- lsp.lua
 
-local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- nil_ls: Nix LSP
-lspconfig.nil_ls.setup({
-  capabilities = capabilities,
-})
-
+vim.lsp.config("nil_ls", {capabilities=capabilities,})
+vim.lsp.enable("nil_ls")
 -- rustaceanvim sets up rust-analyzer on its own via its plugin;
 -- do NOT call lspconfig.rust_analyzer.setup() here.
