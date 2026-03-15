7e6e1d3c08f177bb7951de64ae776add33ca3fdf
crimsonneuron <james.r.devereux@gmail.com>
2026-03-15 16:39:10 -0400

    removed some unnecessary apps hopefully, changed zshrc

--- modules/home-manager/utils/zsh.nix
+++ modules/home-manager/utils/zsh.nix
@@ -9,7 +9,7 @@ in
       
       shellAliases = {
         rebuild = "sudo nixos-rebuild switch" + flakeString;
-        cleanup = "sudo nix-collect-garbage -d";
+        cleanup = "sudo nix-collect-garbage -delete-older-than 7d";
         test = "sudo nixos-rebuild test --impure" +flakeString;
         #The --impure is so that I can test without running a billion git add .s
 
@@ -21,7 +21,9 @@ in
         export VISUAL=nvim
         export DISPLAY=:0
         eval "$(zoxide init zsh)"
+        PROMPT='%F{#DBC713}%n@%m%f:%F{#DBC713}%~$f>'
         alias -- :q=exit
+
         '';
 };
 }
