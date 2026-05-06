f67a0429c8b172cd58ee22fe70a713d1015d871a
crimsonneuron <james.r.devereux@gmail.com>
2026-05-06 00:30:11 -0400

    I'm beginning to fear the output going off the rails

--- modules/nixos/greetd.nix
+++ modules/nixos/greetd.nix
@@ -6,7 +6,7 @@
     settings = {
       terminal.vt = lib.mkForce 2;
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --remember --remember-session --time";
+        command = "${pkgs.tuigreet}/bin/tuigreet --remember --remember-session --time --sessions ${pkgs.niri}/share/wayland-sessions";
         user = "greeter";
       };
 
